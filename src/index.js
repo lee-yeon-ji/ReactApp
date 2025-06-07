4538fa34913058d58c27dce19bf72eb60f05fd69
lee-yeon-ji <duswl2418@naver.com>
2025-06-07 21:26:55 +0900

    react

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from "./reportWebVitals";
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
