54ff36bef6776df5f743c0f1461dcb9bf866e8b5
lee-yeon-ji <duswl2418@naver.com>
2025-06-08 17:16:03 +0900

    react 이전까지 했던 부분 복습

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Button from "./Button";
 
+// css를 연결하고 싶을 때 import 기능역할 from "./적용할 .css"
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
+        {/*css 를 해당 코드에 적용시킬 때 className={기능역할.기능클래스 이름}*/}
       <h1 className={styles.title}>Welcome back!!!!</h1>
         <Button text={"Continue"}/>
 
-        {/*수정 */}
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from "./reportWebVitals";
 import "./styles.css";
 
+// 브라우저 화면이 보여질 부분
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
