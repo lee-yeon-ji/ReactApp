5f7b5d099140b367eed3ddb0fdea5b7ba0c6fdfc
lee-yeon-ji <duswl2418@naver.com>
2025-06-22 22:56:43 +0900

    react todolist 복습

--- src/ToDo_practice/ToDoList.js
+++ src/ToDo_practice/ToDoList.js
@@ -3,22 +3,15 @@ import button from "../Button";
 import {logDOM} from "@testing-library/react";
 
 
-function ToDoList(){
-    //빈배열 생성
-    const [toDo, setToDo] = useState("");
-    const Title = (
-        <h3 id="title" onClick={()=> console.log("click")}>
-            click me
-        </h3>
-    );
+function ToDoList({text}){
 
     return(
 
-            <div>
+            <button>{text}
 
 
 
-            </div>
+            </button>
 
     )
 }
