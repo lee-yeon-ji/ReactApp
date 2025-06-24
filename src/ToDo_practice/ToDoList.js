548575db9ea420e2630b3a5d2623f1570ef3ee29
lee-yeon-ji <duswl2418@naver.com>
2025-06-24 17:54:11 +0900

    react todolist 복습

--- src/ToDo_practice/ToDoList.js
+++ src/ToDo_practice/ToDoList.js
@@ -3,6 +3,79 @@ import button from "../Button";
 
 
 
+
+
+
+
+
+
+function ToDoList(){
+    const [text, setText] = useState("");
+    const [toText, setToText] = useState([]);
+    const onChange = (event) => setText(event.target.value);
+    const onSubmit = (event) => {event.preventDefault();
+        if (text === ""){
+            return;
+        }
+        setToText((currentArray) => [text, ...currentArray]);
+        setText("");
+    };
+    console.log(toText)
+    return(
+        <div>
+            <h1>My To Dos ({toText.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input value={text} onChange={onChange} type="text" placeholder ="write your to do..."/>
+                <button>submit</button>
+            </form>
+        </div>
+    )
+
+}
+
+
+
+
+export default ToDoList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function ToDoList(){
 //
 //     // counter은 데이터 setValues는 데이터를 수정하기 위한 함수 결과값은 counter의 배열.
@@ -36,36 +109,35 @@ import button from "../Button";
 // export default ToDoList;
 
 
-function ToDoList(){
-    const [counter, setCounter] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setCounter((prev) => prev +1);
-    const onChange = (event) => setKeyword(event.target.value);
+// function ToDoList(){
+//     const [counter, setCounter] = useState(0);
+//     const [keyword, setKeyword] = useState("");
+//     const onClick = () => setCounter((prev) => prev +1);
+//     const onChange = (event) => setKeyword(event.target.value);
+//
+//     useEffect(() => {
+//         console.log("start")
+//     },[]);
+//
+//     useEffect(() => {
+//         console.log("counter")
+//     }, [counter]);
+//
+//
+//     useEffect(() => {
+//         if(keyword !==  "" && keyword.length > 5){
+//             console.log("for", keyword);
+//         }
+//
+//
+//         }
+//     ,[keyword])
+//     return (
+//         <div>
+//             <input value={keyword} onChange={onChange} type="text" placeholder="search here..."/>
+//             <h1 >{counter}</h1>
+//             <button onClick={onClick}>click me</button>
+//         </div>
+//     )
+// }
 
-    useEffect(() => {
-        console.log("start")
-    },[]);
-
-    useEffect(() => {
-        console.log("counter")
-    }, [counter]);
-
-
-    useEffect(() => {
-        if(keyword !==  "" && keyword.length > 5){
-            console.log("for", keyword);
-        }
-
-
-        }
-    ,[keyword])
-    return (
-        <div>
-            <input value={keyword} onChange={onChange} type="text" placeholder="search here..."/>
-            <h1 >{counter}</h1>
-            <button onClick={onClick}>click me</button>
-        </div>
-    )
-}
-
-export default ToDoList;
