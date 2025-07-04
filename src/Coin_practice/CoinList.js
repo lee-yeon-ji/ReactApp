0a2285a6bc77dad8ecbb15d54f63d69927fd6215
lee-yeon-ji <duswl2418@naver.com>
2025-07-04 19:33:50 +0900

    react coin data 가져오기 복습

--- src/Coin_practice/CoinList.js
+++ src/Coin_practice/CoinList.js
@@ -1,62 +1,92 @@
-import {useState, useEffect} from "react";
+import {useState} from "react";
 
-function CoinList(){
+function CoinList() {
 
-
-
-        const [loading, setLoading] = useState(true);
-        const [coins, setCoins] = useState([]);
-        const [selCoins, setSelCoins] = useState("0");
-        const [result, setResult] = useState("0");
-
-
-        const onChange = (event) => {
-            setSelCoins(event.target.value);
+    const [todo, setToDo] = useState("");
+    const [toDoList , setToDoList] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {event.preventDefault();
+        if(todo === ""){
+            return;
         }
 
-        const onSubmit = (event) => {
-            event.preventDefault();
-            const priceUSD = event.target[0].value;
-            calculate(priceUSD);
-        }
+        setToDo("");
+        setToDoList((currentArray) => [todo, ...currentArray]);
+    };
 
-        const calculate = (priceUSD) => {
-            setResult(priceUSD * selCoins);
-        }
+    return(
+        <div>
+            <h1>to do list ({toDoList.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={todo} type="text" placeholder="write your to do..."/>
+                <button>submit</button>
+            </form>
+            <hr/>
+            <ul>
+                {toDoList.map((item, index)=> <li key={index}>{item}</li>)}
+            </ul>
+        </div>
+    )
 
 
-        useEffect(() => {
-            fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-                .then((response) => response.json())
-                .then((json) => {
-                    setCoins(json);
-                    setLoading(false);
-                })
-        }, []); // 빈 배열이면 한번만 동작
+}
 
-        return (
-            <div>
-                <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-                {loading
-                    ? <strong>Loading....</strong>
-                    : <select onChange={onChange}>
-                        {coins.map((coin, index) =>
-                            <option
-                                key={index}
-                                value={coin.quotes.USD.price}
-                            >{coin.name} : {coin.quotes.USD.price}
-                            </option>
-                        )}
-                    </select>}
-                <form onSubmit={onSubmit}>
-                    <input type="number" placeholder="write us dollars" />
-                    <button>change coins</button>
-                </form>
-                <h2>you can change {result} coins!</h2>
-            </div>
-        );
-    }
 
+    //
+    //
+    //     const [loading, setLoading] = useState(true);
+    //     const [coins, setCoins] = useState([]);
+    //     const [selCoins, setSelCoins] = useState("0");
+    //     const [result, setResult] = useState("0");
+    //
+    //
+    //     const onChange = (event) => {
+    //         setSelCoins(event.target.value);
+    //     }
+    //
+    //     const onSubmit = (event) => {
+    //         event.preventDefault();
+    //         const priceUSD = event.target[0].value;
+    //         calculate(priceUSD);
+    //     }
+    //
+    //     const calculate = (priceUSD) => {
+    //         setResult(priceUSD * selCoins);
+    //     }
+    //
+    //
+    //     useEffect(() => {
+    //         fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+    //             .then((response) => response.json())
+    //             .then((json) => {
+    //                 setCoins(json);
+    //                 setLoading(false);
+    //             })
+    //     }, []); // 빈 배열이면 한번만 동작
+    //
+    //     return (
+    //         <div>
+    //             <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+    //             {loading
+    //                 ? <strong>Loading....</strong>
+    //                 : <select onChange={onChange}>
+    //                     {coins.map((coin, index) =>
+    //                         <option
+    //                             key={index}
+    //                             value={coin.quotes.USD.price}
+    //                         >{coin.name} : {coin.quotes.USD.price}
+    //                         </option>
+    //                     )}
+    //                 </select>}
+    //             <form onSubmit={onSubmit}>
+    //                 <input type="number" placeholder="write us dollars" />
+    //                 <button>change coins</button>
+    //             </form>
+    //             <h2>you can change {result} coins!</h2>
+    //         </div>
+    //     );
+    // }
+    //
 
 
 //     const [loading, setLoading] = useState(true);
