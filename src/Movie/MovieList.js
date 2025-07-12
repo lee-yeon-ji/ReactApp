48acaca64dac8e3a05562469f9a672021a4b7e62
lee-yeon-ji <duswl2418@naver.com>
2025-07-12 23:19:43 +0900

    react movie 복습

--- src/Movie/MovieList.js
+++ src/Movie/MovieList.js
@@ -3,6 +3,7 @@ import {useState, useEffect} from "react";
 function MovieList(){
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
+    const onsubmit = () => {}
 
     const getMovies = async () => {
         const json = await (
@@ -24,7 +25,10 @@ function MovieList(){
         <div>
             <h1>Movie ranking! :)</h1>
 
-            {loading ? <h1>Loading...</h1>:movies.map(movie => <div key={movie.id}><h2>{movie.title}</h2><p>{movie.summary}</p>
+            {loading ? <h1>Loading...</h1>:movies.map(movie => <div key={movie.id}>
+                <img src={movie.medium_cover_image} alt={movie.title}/>
+                <button onSubmit={onsubmit}>{movie.title}</button>
+                <p>{movie.summary}</p>
             <ul>
                 {movie.genres.map((g) => <li key={g}>{g}</li>)}
             </ul></div>)}
