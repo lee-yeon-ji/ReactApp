c42b222bf096791579af1b37fde663628b0067fd
lee-yeon-ji <duswl2418@naver.com>
2025-07-15 23:30:54 +0900

    react movie 복습

--- src/Movie/MovieList.js
+++ src/Movie/MovieList.js
@@ -1,10 +1,13 @@
-function Movie() {
+function Movie({medium_cover_image, title, summary, genres }) {
     return <div >
-        <img src={medium_cover_image} alt={movie.title}/>
+        <img src={medium_cover_image}/>
+        <h2>{title}</h2>
         <button onSubmit={onsubmit}>{movie.title}</button>
         <p>{summary}</p>
         <ul>
-            {genres.map((g) => <li key={g}>{g}</li>)}
+            {genres.map((g) => (
+                <li key={g}>{g}</li>
+            ))}
         </ul></div>
 }
 export default Movie;
